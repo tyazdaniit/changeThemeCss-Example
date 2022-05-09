b1c6c3868c5759f2e1f8c99577accf1e033b303a
T.Yazadni <56879548+yazdani1994@users.noreply.github.com>
2022-05-09 18:02:41 +0430

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const colorBtns = document.querySelectorAll(".btn");
+
+colorBtns.forEach(function (colorBtn) {
+  colorBtn.addEventListener("click", function (event) {
+    let btnColor = event.target.dataset.color;
+    document.documentElement.style.setProperty("--theme-color", btnColor);
+  });
+});
